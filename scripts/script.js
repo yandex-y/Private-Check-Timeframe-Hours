520b7e4900ad51e805b04f64ab0b751994b92bc4
www <95964290+yandex-y@users.noreply.github.com>
2022-07-16 20:51:35 +0700

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,82 @@
-setInterval(
-function cek(){
-	var hari = new Date();
-	var jamnow = hari.getHours();
-	var menitnow = hari.getMinutes();
-	var inputjam = document.getElementById("inputjam").value;
-	var inputmenit = document.getElementById("inputmenit").value;
-	
-	var calculatemenit = (60-menitnow) + parseInt(inputmenit);
-	
-	if (calculatemenit>59){
-		jamtambahan = 1;
-		calculatemenit = calculatemenit-60;
-		}else{
-			jamtambahan = 0;
-			}
-			
-	if (inputjam>jamnow){
-		var calculatejam = inputjam-jamnow-1+jamtambahan;
-		}else if(inputjam<jamnow){
-			var calculatejam = (23-jamnow) + parseInt(inputjam)+jamtambahan;
-			}else if((inputjam==0) && (inputmenit>menitnow)){
-				calculatejam = 0;
-}else if((inputjam==0) && (inputmenit<menitnow)){
-				var calculatejam = 23;
-}else if((inputjam==0) && (inputmenit==menitnow)){
-				var calculatejam = 24;
-}else if(inputjam=jamnow&&inputmenit==menitnow){
-				var calculatejam = 24;
-				}else if(inputjam=jamnow&&inputmenit>menitnow){
-					var calculatejam = 0;
-					}else if(inputjam=jamnow&&inputmenit<menitnow){
-						var calculatejam = 23;
-						}
-						
-	if((inputjam=="") && (inputmenit=="")){
-		var keluar = ("Masukkan Jam & Menit Terlebih Dahulu");
-		}else if((inputjam=="") && (inputmenit!=="")){
-			var keluar = ("Masukkan Jam Terlebih Dahulu");
-			}else if((inputjam!=="") && (inputmenit=="")){
-				var keluar = ("Masukkan Menit Terlebih Dahulu");
-				}else if((inputjam>23) || (inputmenit>59)){
-					var keluar = ("Masukkan Jam & Menit Dengan Benar");
-					}else if(calculatejam==0){
-						var keluar = ("Tersisa "+calculatemenit+" Menit");
-						}else if(calculatemenit==0){
-							var keluar = ("Tersisa "+calculatejam+" Jam ");
-							}else{
-								var keluar = ("Tersisa "+calculatejam+" Jam "+calculatemenit+" Menit");
-								}
-								
-								
-	document.getElementById("display").innerHTML=keluar;
+//VARIABEL
+const input_hours = document.getElementById('input-hours');
+const input_minutes = document.getElementById('input-minutes');
+const display = document.getElementById('display');
+const header = document.getElementById('header');
 
-	}
-	,213)
-//gabungin dengan h1.js
-//validasi max length
-//validasi angka input
+//EVENT LISTENER
+input_hours.addEventListener('input', (e) => {
+    focusToMinutes(e.target, 2, input_minutes)
+    validasiValue(e.target, 23)
+    logic()
+})
+input_minutes.addEventListener('input', (e) => {
+    validasiValue(e.target, 59)
+    logic()
+})
+input_minutes.addEventListener('keydown', (e) => {
+    focusToHours(e, input_hours)
+})
+
+//RUN FUNCTION
+logic()
+timeCondition()
+
+//FUNCTION
+
+function validasiValue(target, maxvalue) {
+    if (target.value > maxvalue) {
+        target.value = maxvalue
+    }
+}
+
+function logic() {
+    const getDateNow = new Date().getTime();
+    const getDateTarget = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), parseInt(input_hours.value), parseInt(input_minutes.value), new Date().getSeconds(), new Date().getMilliseconds()).getTime();
+    let calc = getDateTarget - getDateNow;
+
+    if (calc < 0) {
+        calc = calc + 86400000
+    }
+
+    const toHours = Math.floor(calc / 1000 / 60 / 60);
+    const toMinutes = Math.floor((calc / 1000 / 60) - toHours * 60);
+
+    if (input_hours.value.length == 0 || input_minutes.value.length == 0) {
+        display.innerHTML = 'Masukkan Input Terlebih Dahulu'
+    } else if (toHours == 0 && toMinutes == 0) {
+        display.innerHTML = 'Now'
+    } else {
+        display.innerHTML = `Kurang ${toHours == 0 ? '': toHours + ' Jam'} ${toMinutes == 0 ? '': toMinutes + ' Menit'} Lagi`
+    }
+}
+
+function timeCondition() {
+    const getHours = new Date().getHours();
+    let con;
+
+    if (getHours < 11) {
+        con = 'Pagi'
+    } else if (getHours < 15) {
+        con = 'Siang'
+    } else if (getHours < 19) {
+        con = 'Sore'
+    } else {
+        con = 'Malam'
+    }
+
+    header.innerHTML = 'Selamat ' + con
+}
+
+function focusToMinutes(target, maxlength, next) {
+    if (target.value.length > 1) {
+        next.focus()
+    }
+}
+function focusToHours(e, to) {
+    if (e.key == 'Backspace') {
+        if (e.target.value.length == 0) {
+            to.focus()
+        }
+    }
+}
